bab32f15d3da6c59baa0aa91c9049bfb923b37d8
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-10-30 00:51:05 +0800

    Added crsscraper.py

--- crs_main.py
+++ crs_main.py
@@ -1,6 +1,21 @@
 from crs_data import Data
 from data_sorter import DataSorter, ScheduleGenerator, ListOfCoursesWithTime
+# from crscraper import CRScraper
 
+# login_url = "https://crs.upd.edu.ph/"
+# all_course_table_schedule_url = ["https://crs.upd.edu.ph/student_registration/class_search/5670", 
+#                                         "https://crs.upd.edu.ph/student_registration/class_search/18849", 
+#                                         "https://crs.upd.edu.ph/student_registration/class_search/18843",
+#                                         "https://crs.upd.edu.ph/student_registration/class_search/19401",
+#                                         "https://crs.upd.edu.ph/student_registration/class_search/19395"
+#                                         ]
+
+# crs_username = input("Enter CRS Username: ")
+# crs_password = input("Enter CRS Password: ")
+
+# crs_scraper = CRScraper(login_url, crs_username, crs_password, all_course_table_schedule_url)
+
+# data = crs_scraper.main() # This should replace the data variable
 data_class = Data()
 data: list[dict[str, str | list[str]]] = data_class.data()
 
