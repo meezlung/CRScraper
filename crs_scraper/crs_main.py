2eb3e13a891c78747f20222da47e16d121720933
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-11-06 00:35:52 +0800

    Add preparation for Flask & Svelte implementation

--- crs_scraper/crs_main.py
+++ crs_scraper/crs_main.py
@@ -1,14 +1,17 @@
 from crs_data import Data
 from data_sorter import DataSorter, ScheduleGenerator, ListOfCoursesWithTime
+
+
+# ------------------------------------------------------------
 # from crscraper import CRScraper
 
 # login_url = "https://crs.upd.edu.ph/"
 # all_course_table_schedule_url = ["https://crs.upd.edu.ph/student_registration/class_search/5670", 
-#                                         "https://crs.upd.edu.ph/student_registration/class_search/18849", 
-#                                         "https://crs.upd.edu.ph/student_registration/class_search/18843",
-#                                         "https://crs.upd.edu.ph/student_registration/class_search/19401",
-#                                         "https://crs.upd.edu.ph/student_registration/class_search/19395"
-#                                         ]
+                                 # "https://crs.upd.edu.ph/student_registration/class_search/18849", 
+                                 # "https://crs.upd.edu.ph/student_registration/class_search/18843",
+                                 # "https://crs.upd.edu.ph/student_registration/class_search/19401",
+                                 # "https://crs.upd.edu.ph/student_registration/class_search/19395"
+                                 # ]
 
 # crs_username = input("Enter CRS Username: ")
 # crs_password = input("Enter CRS Password: ")
@@ -16,6 +19,9 @@ from data_sorter import DataSorter, ScheduleGenerator, ListOfCoursesWithTime
 # crs_scraper = CRScraper(login_url, crs_username, crs_password, all_course_table_schedule_url)
 
 # data = crs_scraper.main() # This should replace the data variable
+# ------------------------------------------------------------
+
+
 data_class = Data()
 data: list[dict[str, str | list[str]]] = data_class.data()
 
@@ -26,4 +32,22 @@ data_sorter.sort_data()
 data_generator = ScheduleGenerator(data_sorter.subjects_with_time)
 schedules: list[ListOfCoursesWithTime] = data_generator.generate_schedules(data_sorter.subjects_with_time)
 data_generator.display_all_possible_schedules(schedules)
-# data_generator.convert_to_csv(schedules)
+data_generator.convert_to_csv(schedules)
+
+
+# ------------------------------------------------------------
+# from flask import Flask, jsonify
+# import csv
+
+# app = Flask(__name__)
+
+# @app.route('/get-schedule', methods=['GET'])
+# def get_schedule():
+#     with open('schedules.csv', 'r') as file:
+#         reader = csv.DictReader(file)
+#         schedules = [row for row in reader]
+#     return jsonify(schedules)
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=8080)
+# ------------------------------------------------------------
