f985e30e12efe2da32588793b964d69b6e4ed68a
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-12-18 17:29:25 +0800

    Added print debug statements

--- test.py
+++ test.py
@@ -7,9 +7,10 @@
 # https://crs.upd.edu.ph/preenlistment/class_search/19405, https://crs.upd.edu.ph/preenlistment/class_search/19398, https://crs.upd.edu.ph/preenlistment/class_search/19403, https://crs.upd.edu.ph/preenlistment/class_search/19404, https://crs.upd.edu.ph/preenlistment/class_search/19480
 
 # from crs_scraper.crscraper_preenlistment import CRScraper
-from crs_scraper.data_sorter import DataSorter, ScheduleGenerator 
+from crs_scraper.data_sorter import ScheduleGenerator 
 from crs_scraper.optimized_crscraper import CRScraper
 from getpass import getpass # This is used to hide the password input when typing in the terminal (https://docs.python.org/3/library/getpass.html)
+import json
 
 login_url = "https://crs.upd.edu.ph/"
 crs_username_global = input("CRS Username: ")
@@ -34,10 +35,15 @@ all_course_table_schedule_url_test = [""]
 
 filename = "schedules_ranked_test.csv"
 
-crscraper = CRScraper(login_url, crs_username_global, crs_password_global, all_course_table_schedule_url_test)
+crscraper = CRScraper(login_url, crs_username_global, crs_password_global, all_course_table_schedule_url_cs)
 data = crscraper.main()
 
-print(data)
+print("Data has been scraped succesfully.")
+print()
+
+# Print data beautifully
+print(json.dumps(data, indent=1))
+print()
 
 if data:
     data_generator = ScheduleGenerator(data)
@@ -48,15 +54,5 @@ if data:
     # data_generator.display_all_possible_schedules(ranked_schedules)
     data_generator.convert_to_csv(ranked_schedules, filename)
 
-# if data:
-#     data_sorter = DataSorter(data)
-#     data_sorter.sort_data()
-#     # data_sorter.display_data(data_sorter.subjects_with_time)
-
-#     data_generator = ScheduleGenerator(data_sorter.subjects_with_time)
-#     schedules = data_generator.generate_schedules(data_sorter.subjects_with_time)
-#     # data_generator.display_all_possible_schedules(schedules)
-
-#     ranked_schedules = data_generator.rank_by_probability(schedules)
-#     # data_generator.display_all_possible_schedules(ranked_schedules)
-#     data_generator.convert_to_csv(ranked_schedules, filename)
+    print(f"Ranked schedules saved to {filename}.")
+    print()
