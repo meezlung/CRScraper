eabccb46377d269fd7c3ea71c03eaa02e3aaff4b
meezlung <gabrielmislang101@gmail.com>
2025-06-27 09:07:39 +0800

    fix: remove unnecessary libraries

--- crs_main.py
+++ crs_main.py
@@ -32,7 +32,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-import pyotp
 
 # ------------------------------------------------------------
 app = Flask("CRScraper") 
